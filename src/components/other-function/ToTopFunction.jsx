87d8532751c94b7c2c16a8cb0df3e776b1d51003
DingoPham <pencibambo@gmail.com>
2024-08-19 15:49:26 +0700

    finished edit the container funtion

--- src/components/other-function/ToTopFunction.jsx
+++ src/components/other-function/ToTopFunction.jsx
@@ -2,11 +2,8 @@ import React, {useState, useEffect} from 'react';
 import '../../assets/Style/Function.css';
 
 function ToTopFunc(){
-    const [isVisible, setIsVisible] = useState(false);
-
-    const toggleVisibility = () =>{
-        setIsVisible(true)
-    };
+    const [isVisible, setIsVisible] = useState(false)
+    const [isHover, setIsHover] = useState(false)
 
     const toTop = () => {
         window.scrollTo({
@@ -16,21 +13,37 @@ function ToTopFunc(){
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', toggleVisibility);
+        const handleScroll = () => {
+            const scrollPostion = window.pageYOffset;
+            setIsVisible(scrollPostion > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
         return () => {
-            window.removeEventListener('scroll', toggleVisibility);
+            window.removeEventListener('scroll', handleScroll)
         };
     }, []);
 
     return(
         <div>
-            {isVisible && (
-                <button onClick={toTop} className='scroll-to-top'>
-                    <i className="fas fa-caret-up tx-c c-black"></i>
-                </button>
-        )}
+           <button className='scroll-to-top'
+                   style={styleTop(isVisible , isHover)}
+                   onClick={toTop}
+                   disabled={!isVisible}
+                   onMouseEnter={() => setIsHover(true)}
+                   onMouseLeave={() => setIsHover(false)}
+                   >
+                <i className="fas fa-caret-up tx-c c-black"></i>
+            </button>
         </div>
-    )
-}
+    );
+}   
+
+const styleTop = (isVisible, isHover) => ({
+    backgroundColor: isHover ? '#ffffff' : isVisible ? '#00000060' : '#1d1d1d',
+    cursor: isVisible ? 'pointer' : 'not-allowed',
+    opacity: isVisible ? '1' : '0.5'
+}); 
+
 
 export default ToTopFunc;
