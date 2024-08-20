df98eb0eb6e943a16dbe0206e49fbb85250d98da
DingoPham <pencibambo@gmail.com>
2024-08-20 12:00:37 +0700

    rename for respository5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path='/' element={<Home/>} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
 }
 
