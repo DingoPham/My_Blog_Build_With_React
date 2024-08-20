27ac25f5695286dc657d49ecc6b358eea8979aae
DingoPham <pencibambo@gmail.com>
2024-08-20 14:20:10 +0700

    rename for respository8

--- src/components/home-page/Foot.jsx
+++ src/components/home-page/Foot.jsx
@@ -1,16 +1,12 @@
 import '../../assets/Style/Footer.css';
 import AnimatedContent from '../other-function/AnimatedContent';
 
-import React from 'react';
-import { useTranslation } from '../other-function/TranslationList';
-
 const Foot = () =>{
-    const {translations, language} = useTranslation();
     return(
         <div className="flex foot tx-c flex-column">
             <AnimatedContent>
             <div className='p-2 flex flex-column'>
-                <p>{translations[language].footer}</p>
+                <p>If you have any questions, write to me via my gmail:</p>
                 <p className="f-30 flex space-between">pencibambo@gmail.com
                 </p>
             </div>
